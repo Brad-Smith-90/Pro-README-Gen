f3469ca8067445252d2e4ef1af04e03a8364bd4f
Brad Smith <b.smith66090@gmail.com>
2024-08-23 13:32:20 -0400

    :sparkles: feat(prompt) add functioning user prompt

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 import inquirer from 'inquirer';
-import fs from 'fs';
+
+
+// Function to prompt user for information
+async function promptUser() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: "What's the title of your project?"
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:'
+    }
+    ]);
+}
+
+
+async function init() {
+  try {
+    const answers = await promptUser();
+    console.log(answers);
+  } catch (err) {
+    console.error(error);
+  }
+}
+
+init();
