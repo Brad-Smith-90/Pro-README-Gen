ca3fadb2342ee0f4ae2b019303e3618c0c4780ef
Brad Smith <b.smith66090@gmail.com>
2024-08-24 15:43:59 -0400

    :sparkles: Feat:(Generate Folder/File):sparkles:
    Readme Generator no longer overwrites project README, Instead, it Generates the Gen-README folder(if one does not exist) in the current working directory then generates the Readme in that folder with whichever file name you input in the prompt or the default name: README.md

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import inquirer from 'inquirer';
 import fs from 'fs';
+import path from 'path';
 import { generateMarkdown } from './markdown.js';
 
 // Function to prompt user for information
@@ -16,58 +17,56 @@ async function promptUser() {
       message: 'Provide a description of your project:'
     },
     {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of your project:'
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'How do you install your project?'
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide instructions and examples for use:'
-      },
-      {
-        type: 'input',
-        name: 'contributing',
-        message: 'Provide guidelines on how others can contribute to your project:'
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'Provide examples on how to run tests for your project:'
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license for your project:',
-        choices: ['MIT', 'GPLv2', 'Apache', 'BSD 3-Clause', 'None']
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub username:'
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address:'
-      }
-    ];
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install your project?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions and examples for use:'
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide guidelines on how others can contribute to your project:'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide examples on how to run tests for your project:'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GPLv2', 'Apache', 'BSD 3-Clause', 'None']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:'
+    },
+    {
+      type: 'input',
+      name: 'filename',
+      message: 'Enter the filename for the generated README (default: README.md):',
+      default: 'README.md'  // Default filename
+    }
+  ];
 
+  let answers = await inquirer.prompt(questions);
+  let isConfirmed = false;
 
-let answers = await inquirer.prompt(questions);
-let isConfirmed = false;
+  while (!isConfirmed) {
+    console.log('\nReview your answers:');
+    console.log(answers);
 
-while (!isConfirmed) {
-  console.log('\nReview your answers:');
-  console.log(answers);
-    
-
-     // Ask if the answers are correct
     const { confirm } = await inquirer.prompt({
       type: 'confirm',
       name: 'confirm',
@@ -77,7 +76,6 @@ while (!isConfirmed) {
     if (confirm) {
       isConfirmed = true;
     } else {
-      // Ask which answer they want to change
       const { keyToChange } = await inquirer.prompt({
         type: 'list',
         name: 'keyToChange',
@@ -85,12 +83,11 @@ while (!isConfirmed) {
         choices: [...Object.keys(answers), 'None'],
       });
 
-      
       if (keyToChange !== 'None') {
         const updatedAnswer = await inquirer.prompt(questions.find(q => q.name === keyToChange));
         answers[keyToChange] = updatedAnswer[keyToChange];
       } else {
-        isConfirmed = true; // If the user selects 'None', exit the loop
+        isConfirmed = true;
       }
     }
   }
@@ -100,18 +97,27 @@ while (!isConfirmed) {
 
 
 function writeToFile(fileName, data) {
-  fs.writeFileSync(fileName, data);
+  const targetDir = path.join(process.cwd(), 'Gen-README'); 
+  const filePath = path.join(targetDir, fileName);
+  
+ 
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true }); 
+  }
+
+  
+  fs.writeFileSync(filePath, data, 'utf8');
+  console.log(`Successfully generated ${filePath}`);
 }
 
-
+// Function to initialize the app
 async function init() {
   try {
     const answers = await promptUser();
     const markdown = generateMarkdown(answers);
-    writeToFile('README.md', markdown);
-    console.log('Generate README.md...Success!');
+    writeToFile(answers.filename, markdown); 
   } catch (err) {
-    console.error('Error! Unable to generate README.md.');
+    console.error('Error! Unable to generate README.', err); 
   }
 }
 
